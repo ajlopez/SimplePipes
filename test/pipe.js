094e19594a3c8a34e4413fb4a13bee1f01eb5d12
ajlopez <webmaster@ajlopez.com>
2013-01-08 22:09:54 -0300

    Pipe count with test

--- test/pipe.js
+++ test/pipe.js
@@ -22,3 +22,11 @@ exports['sends and receives'] = function (test) {
     test.equal(pipe.receive(), 2);
     test.done();
 };
+
+exports['count'] = function (test) {
+    var pipe = simplepipes.createPipe();
+    pipe.send(1);
+    pipe.send(2);
+    test.equal(pipe.count(), 2);
+    test.done();
+};
