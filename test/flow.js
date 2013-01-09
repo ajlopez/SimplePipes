2a73f7011bd4ed4fe60582f0724107d85b2017f4
ajlopez <webmaster@ajlopez.com>
2013-01-08 22:42:53 -0300

    listen pipe test

--- test/flow.js
+++ test/flow.js
@@ -17,3 +17,19 @@ exports['start free functions'] = function (test) {
 
     flow.start();
 };
+
+exports['listen pipe'] = function (test) {
+    test.expect(1);
+
+    var flow = simplepipes.createFlow(
+        "pipe1",
+        function (val1) {
+            console.log('enter');
+            test.equal(val1, 1);
+            test.done();
+        }
+    );
+
+    flow.start();
+    flow.pipe1.send(1);
+};
