4ae01675fb85be9c68c099d7346e0856f9233382
ajlopez <webmaster@ajlopez.com>
2014-09-03 10:47:14 -0300

    Simplyfing lib, use simpleunit

--- test/pipe.js
+++ test/pipe.js
@@ -1,32 +1,103 @@
 
-var simplepipes = require('..');
+var pipe = require('../lib/pipe');
 
-exports['createPipe defined'] = function (test) {
-    test.ok(simplepipes.createPipe);
-    test.equal(typeof simplepipes.createPipe, 'function');
-    test.done();
+exports['create and post to pipe'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 1);
+        test.done();
+    });
+    
+    p.post(1);
 };
 
-exports['send and receive'] = function (test) {
-    var pipe = simplepipes.createPipe();
-    pipe.send(1);
-    test.equal(pipe.receive(), 1);
-    test.done();
+exports['create pipe, chain with pipe, post'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg) { return msg + 1; });
+    
+    p.pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 2);
+        test.done();
+    });
+    
+    p.post(1);
 };
 
-exports['sends and receives'] = function (test) {
-    var pipe = simplepipes.createPipe();
-    pipe.send(1);
-    pipe.send(2);
-    test.equal(pipe.receive(), 1);
-    test.equal(pipe.receive(), 2);
-    test.done();
+exports['create pipe, chain with process and pipe, post'] = function (test) {
+    var value = 0;
+    
+    test.async();
+    
+    var p = pipe(function (msg) { return msg + 1; });
+    
+    p
+    .process(function (msg) {
+        test.equal(msg, 2);
+        value = msg;
+    })
+    .pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 2);
+        test.equal(value, 2);
+        test.done();
+    });
+    
+    p.post(1);
 };
 
-exports['count'] = function (test) {
-    var pipe = simplepipes.createPipe();
-    pipe.send(1);
-    pipe.send(2);
-    test.equal(pipe.count(), 2);
-    test.done();
+exports['create pipe, chain with pipe using async'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg, next) { next(null, msg + 1); });
+    
+    p.pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 2);
+        test.done();
+    });
+    
+    p.post(1);
+};
+
+exports['emit to next pipe'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg) { this.emit(msg + 1); });
+    
+    p.pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 2);
+        test.done();
+    });
+    
+    p.post(1);
+};
+
+exports['emit to named pipe'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg) { 
+        if (msg % 2)
+            this.emit("odd", msg);
+        else
+            this.emit("even", msg);
+    });
+    
+    p
+    .pipe("even", pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg % 2, 0);
+    }))
+    .pipe("odd", pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg % 2, 1);
+        test.done();
+    }));
+    
+    p.post(2);
+    p.post(1);
 };
