e10ec32fcd5ae574f43df5473d35872acd4cbbfd
ajlopez <webmaster@ajlopez.com>
2014-09-03 10:30:37 -0300

    Pipe to pipe

--- test/pipe2.js
+++ test/pipe2.js
@@ -4,9 +4,25 @@ var pipe = require('../lib/pipe');
 exports['create and post to pipe'] = function (test) {
     test.async();
     
-    pipe(function (msg) {
+    var p = pipe(function (msg) {
         test.ok(msg);
         test.equal(msg, 1);
         test.done();
-    }).post(1);
+    });
+    
+    p.post(1);
+};
+
+exports['create pipe, chain with pipe, post'] = function (test) {
+    test.async();
+    
+    var p = pipe(function (msg) { return msg + 1; });
+    
+    p.pipe(function (msg) {
+        test.ok(msg);
+        test.equal(msg, 2);
+        test.done();
+    });
+    
+    p.post(1);
 };
